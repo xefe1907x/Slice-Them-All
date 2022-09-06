42f39af529cb3d2eb2ca470d034922c4ea69ac66
xefe1907x <efeaslan1995@hotmail.com>
2022-09-06 14:03:01 +0300

    Slice mechanics solved, knife doesnt turn, it's bug, need to solve. First screen Legos completed

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,16 @@
+using System;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target;
+
+    public float smoothSpeed = 0.125f;
+
+    public Vector3 offset;
+
+    void LateUpdate()
+    {
+        transform.position = target.position + offset;
+    }
+}
--- Assets/Scripts/KnifeController.cs
+++ Assets/Scripts/KnifeController.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using DG.Tweening;
 using UnityEngine;
 using UnityEngine.UIElements;
 
@@ -8,30 +9,37 @@ public class KnifeController : MonoBehaviour
 {
     Rigidbody rb;
 
-    [SerializeField] float jumpForce = 10000000f;
+    [SerializeField] float jumpForce = 9000000f;
     [SerializeField] float rightForce = 70000f;
-    [SerializeField] float rotateZ = 550f;
+    float rotateX;
 
     bool isKnifeFlipping;
+    bool isRotateOK = true;
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        DOTween.Init();
     }
 
     void Update()
     {
         JumpKnife();
-        Debug.Log(isKnifeFlipping);
+        FlipKnife();
+        Debug.Log(transform.rotation.x);
     }
 
     void JumpKnife()
     {
         if (Input.GetMouseButtonDown(0))
         {
-            // rb.AddForce(Vector3.up * Time.deltaTime * jumpForce);
-            // rb.AddForce(Vector3.right * Time.deltaTime * rightForce);
-            isKnifeFlipping = true;
-            FlipKnife();
+            if (!isRotateOK)
+                isRotateOK = true;
+            if (!isKnifeFlipping)
+                isKnifeFlipping = true;
+            if (rb.isKinematic)
+                rb.isKinematic = false;
+            rb.AddForce(Vector3.up * Time.deltaTime * jumpForce);
+            rb.AddForce(Vector3.right * Time.deltaTime * rightForce);
         }
     }
 
@@ -39,15 +47,37 @@ public class KnifeController : MonoBehaviour
     {
         if (isKnifeFlipping)
         {
-            transform.Rotate(transform.rotation.x, transform.rotation.y,
-                transform.rotation.z - rotateZ * Time.deltaTime);
-            StartCoroutine(nameof(KnifeStopper));
+            if (isRotateOK)
+                rotateX = 10000f;
+            
+            transform.DORotate(new Vector3 (transform.rotation.x + rotateX * Time.deltaTime, -0.309f, 0.636f), 0.3f, RotateMode.LocalAxisAdd);
+            KnifeStopper();
+        }
+    }
+    void KnifeStopper()
+    {
+        if (isKnifeFlipping)
+            Invoke(nameof(RotateStoper),0.1f);
+        
+        if (rotateX > 5000)
+        {
+            rotateX -= 10000 * Time.deltaTime;
+        }
+
+        else
+        {
+            // 0.0983 - 0.16
+            if (transform.rotation.x >= 0.0983f & transform.rotation.x <= 0.16f)
+            {
+                rotateX = 0;
+                isKnifeFlipping = false;
+            }
         }
     }
 
-    IEnumerator KnifeStopper()
+    void RotateStoper()
     {
-        yield return new WaitForSeconds(3f);
-        isKnifeFlipping = false;
+        if (isRotateOK)
+            isRotateOK = false;
     }
 }
