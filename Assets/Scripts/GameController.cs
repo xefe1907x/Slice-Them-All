e7b866136fdfef004eb080c1b99e192a727bb379
xefe1907x <efeaslan1995@hotmail.com>
2022-09-06 15:15:32 +0300

    finished level bar in start screen

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,14 +1,28 @@
 using System.Collections;
+using TMPro;
 using UnityEngine;
 
 public class GameController : MonoBehaviour
 {
     public GameObject tapText;
+    public GameObject levelCanvas;
+
+    public TextMeshProUGUI levelText;
+
+    int gameLevel = 1;
 
     void Update()
     {
+        LevelCounter();
         TapTextDisabler();
     }
+
+    void LevelCounter()
+    {
+        //Counts level, and writes it to level text
+        levelText.text = "Level: " + gameLevel;
+    }
+
 
     void TapTextDisabler()
     {
@@ -19,6 +33,17 @@ public class GameController : MonoBehaviour
                 if (Input.GetMouseButtonDown(0))
                 {
                     tapText.SetActive(false);
+                }
+            }
+        }
+
+        if (levelCanvas != null)
+        {
+            if (levelCanvas.activeInHierarchy)
+            {
+                if (Input.GetMouseButtonDown(0))
+                {
+                    levelCanvas.SetActive(false);
                 }
             }
         }
