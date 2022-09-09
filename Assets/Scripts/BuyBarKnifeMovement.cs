591041f591342657a91d211510193694595eee61
xefe1907x <efeaslan1995@hotmail.com>
2022-09-09 16:10:29 +0300

    BuyButton bıçak animasyonu bitti

--- Assets/Scripts/BuyBarKnifeMovement.cs
+++ Assets/Scripts/BuyBarKnifeMovement.cs
@@ -1,51 +1,95 @@
+using System;
+using DG.Tweening;
 using UnityEngine;
 
 public class BuyBarKnifeMovement : MonoBehaviour
 {
     float mainPositionx;
     float moveX = 80f;
+    float rotateZ = 25f;
+    float timeForKnife;
+    
     bool canGoRight;
-    bool canGoLeft;
+    bool canTurnRight;
+    bool timeResetter = false;
 
     void Start()
     {
+        DOTween.Init();
         canGoRight = true;
+        canTurnRight = true;
+        timeForKnife = 0;
         mainPositionx = transform.position.x;
-        InvokeRepeating(nameof(MoveKnife),3f,5f);
-        Debug.Log(transform.position.x);
+    }
+
+    void Update()
+    {
+        MoveKnife();
     }
 
     void MoveKnife()
     {
+        //transform.position
         //Start: 129.57
         //End: 184.86
-
-        if (canGoRight)
+        
+        timeForKnife += Time.deltaTime;
+        
+        if (canGoRight & timeForKnife >= 3f)
         {
             if (transform.position.x <= 184.86f)
             {
                 transform.position = new Vector3(transform.position.x + moveX * Time.deltaTime, transform.position.y,
                     transform.position.z);
-
-                if (transform.position.x >= 184.86f)
-                {
-                    canGoRight = false;
-                    canGoLeft = true;
-                }
+            }
+            
+            if (transform.position.x >= 184.86f)
+            {
+                TurnKnife();
             }
         }
         
         else
         {
-            if (transform.position.x >= 129.57f)
+            if (transform.position.x >= mainPositionx)
             {
                 transform.position = new Vector3(transform.position.x - moveX * Time.deltaTime, transform.position.y,
                     transform.position.z);
             }
             
-            if (transform.position.x <= 129.57f)
+            if (transform.position.x <= mainPositionx)
             {
                 canGoRight = true;
+                canTurnRight = true;
+                if (timeResetter)
+                {
+                    timeForKnife = 0;
+                    timeResetter = false;
+                }
+            }
+        }
+    }
+
+    void TurnKnife()
+    {
+        if (canTurnRight & canGoRight)
+        {
+            transform.DORotate(new Vector3(transform.rotation.x, transform.rotation.y, -55), 0.7f);
+            
+            if (transform.rotation.z >= 0.455f)
+            {
+                canTurnRight = false;
+            }
+        }
+
+        else
+        {
+            transform.DORotate(new Vector3(transform.rotation.x, transform.rotation.y, 0), 0.7f);
+
+            if (transform.rotation.z >= 0)
+            {
+                canGoRight = false;
+                timeResetter = true;
             }
         }
     }
