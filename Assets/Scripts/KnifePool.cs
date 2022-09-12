f4719d1cc879aa18bb6b86a733f9bb0da353c9a5
xefe1907x <efeaslan1995@hotmail.com>
2022-09-12 17:46:06 +0300

    Adding some knives

---
+++ Assets/Scripts/KnifePool.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Android;
+
+public class KnifePool : MonoBehaviour
+{
+    int knifeNumber;
+
+    bool isknifeInstantiated;
+
+    public GameObject knifePrefab1;
+
+    void Start()
+    {
+        knifeNumber = 1; // TODO: playprefste kaydet hangi bıçak kullanılıyo ona göre instantiate et
+    }
+
+    void Update()
+    {
+        KnifeSelector();
+    }
+    
+    void KnifeSelector()
+    {
+        if (!isknifeInstantiated)
+        {
+            switch (knifeNumber)
+            {
+                case 1:
+                    Instantiate(knifePrefab1, new Vector3(6.33f, 4.06f, 0f), Quaternion.Euler(122.83f,87.1f,-1.49f));
+                    isknifeInstantiated = true;
+                    break;
+            }
+        }
+    }
+}
