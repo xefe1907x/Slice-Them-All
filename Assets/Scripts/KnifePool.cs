a2fde00a0f275cd9bb27df7927d815710e2b8353
xefe1907x <efeaslan1995@hotmail.com>
2022-09-20 12:44:45 +0300

    Trying to restart game when blade touches respawn

--- Assets/Scripts/KnifePool.cs
+++ Assets/Scripts/KnifePool.cs
@@ -23,7 +23,7 @@ public class KnifePool : MonoBehaviour
     {
         KnifeCreator();
     }
-    
+
     void KnifeCreator()
     {
         if (!isknifeInstantiated)
@@ -31,19 +31,19 @@ public class KnifePool : MonoBehaviour
             switch (knifeNumber)
             {
                 case 0:
-                    Instantiate(knifePrefab1, new Vector3(6.32f, 4.45f, 0f), Quaternion.Euler(115f,90f,0f));
+                    Instantiate(knifePrefab1, new Vector3(6.32f, 4.45f, 0f), Quaternion.Euler(115f, 90f, 0f));
                     isknifeInstantiated = true;
                     break;
                 case 1:
-                    Instantiate(knifePrefab2, new Vector3(6.73f, 5.65f, 0f), Quaternion.Euler(-416.36f,86.47f,-0.7f));
+                    Instantiate(knifePrefab2, new Vector3(6.73f, 5.65f, 0f), Quaternion.Euler(-416.36f, 86.47f, -0.7f));
                     isknifeInstantiated = true;
                     break;
                 case 2:
-                    Instantiate(knifePrefab3, new Vector3(6.68f, 5.5f, 0f), Quaternion.Euler(-59.51f,86.15f,-4.17f));
+                    Instantiate(knifePrefab3, new Vector3(6.68f, 5.5f, 0f), Quaternion.Euler(-59.51f, 86.15f, -4.17f));
                     isknifeInstantiated = true;
                     break;
                 case 3:
-                    Instantiate(knifePrefab4, new Vector3(6.46f, 5.12f, 0f), Quaternion.Euler(60.3f,-90f,0f));
+                    Instantiate(knifePrefab4, new Vector3(6.46f, 5.12f, 0f), Quaternion.Euler(60.3f, -90f, 0f));
                     isknifeInstantiated = true;
                     break;
             }
---
+++ Assets/Scripts/RespawnScript.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RespawnScript : MonoBehaviour
+{
+    int currentLevel;
+    void Start()
+    {
+        currentLevel = SceneManager.GetActiveScene().buildIndex;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Blade"))
+        {
+            RestartLevel();
+            Invoke(nameof(LoadScene),1f);
+        }
+    }
+
+    void LoadScene()
+    {
+        SceneManager.LoadScene(currentLevel);
+    }
+
+    void RestartLevel()
+    {
+        GameController.isGameStarted = false;
+        KnifePool.isknifeInstantiated = false;
+        KnifeBar.isBarInstantiated = true;
+    }
+}
